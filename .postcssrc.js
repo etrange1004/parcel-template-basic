608976b4e63e9274a7ca0bdfc142c2a33035f022
etrange1004 <etrange1004@gmail.com>
2022-03-12 15:47:42 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommonJS - node.js 환경에서 사용.
+
+// import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+// export { 
+//   plugins: [
+//     autoprefixer
+//   ] 
+// }
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
